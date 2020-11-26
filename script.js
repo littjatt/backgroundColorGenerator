d7d1b09ff650b7968d07e9919035f66209abe748
Litt Jatt <73443937+littjatt@users.noreply.github.com>
2020-11-25 22:54:36 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var color3 = document.querySelector(".color3");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value
+	+ ", " 
+	+ color3.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+color3.addEventListener("input", setGradient);
